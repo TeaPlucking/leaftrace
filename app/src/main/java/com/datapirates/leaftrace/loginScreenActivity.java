d18e7583727450872cb8d3c3531bac262244cc53
J A Landsberger <96688702+J232j@users.noreply.github.com>
2023-12-03 13:17:29 +0530

    Update loginScreenActivity.java
    Login Validation for tea pluckers and managers in the tea Plantation.
    
    Co-Authored-By: Anjana Nadun <anjananadun321@gmail.com>

--- app/src/main/java/com/datapirates/leaftrace/loginScreenActivity.java
+++ app/src/main/java/com/datapirates/leaftrace/loginScreenActivity.java
@@ -7,15 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class loginScreenActivity extends AppCompatActivity {
     TextView signHere, forgetpwd;
+    TextView enter_Username, enter_Password;
     Button login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        // Validation of the Login Screen
+
+        enter_Username = findViewById(R.id.login_input);
+        enter_Password = findViewById(R.id.password_input1);
+
+
 
         signHere = findViewById(R.id.signupHerebtn);
         login = findViewById(R.id.login);
@@ -31,7 +40,21 @@ public class loginScreenActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openDashboard();
+                if (enter_Username.getText().toString().equals("admin") && enter_Password.getText().toString().equals("admin")) {
+                    // Correct credentials for tea pluckers
+                    Toast.makeText(loginScreenActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(loginScreenActivity.this, DashboardActivity.class);
+                    startActivity(intent);
+                } else if (enter_Username.getText().toString().equals("plantation staff") && enter_Password.getText().toString().equals("2001")) {
+                    // Correct credentials for plantation staff
+                    Toast.makeText(loginScreenActivity.this, "Login Successful!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(loginScreenActivity.this,mgrDashboardActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Incorrect credentials
+                    Toast.makeText(loginScreenActivity.this, "Incorrect Login Credentials", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
 
